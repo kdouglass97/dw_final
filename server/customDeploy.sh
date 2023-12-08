6b32fc9bd6b2a79bf87c098320a291f117217dac
kdouglass97 <66433917+kdouglass97@users.noreply.github.com>
2023-12-08 08:07:44 -0500

    cool script works to tie together both servers updates W moose.net

---
+++ server/customDeploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Deploy using Vercel and capture the output
+DEPLOY_OUTPUT=$(vercel --prod)
+
+# Extract the URL from the output
+PROD_URL=$(echo "$DEPLOY_OUTPUT" | grep -o "https://[^ ]*\.app")
+
+# Navigate up one directory
+cd ..
+
+# Enter the 'client' directory
+cd client
+# Define the file path
+FILE=".env"
+
+# Check if the file exists
+if [ -f "$FILE" ]; then
+    # If it exists, remove it
+    #rm "$FILE"
+    echo "File $FILE found."
+fi 
+
+# Save the URL into a file
+echo "NEXT_PUBLIC_PROD_URL=\"$PROD_URL\"" > .env
+cd ..
+
+# Call firebase deploy
+sudo firebase deploy
